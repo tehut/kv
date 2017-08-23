9ce8e63ec36d158ae68830e8e644ccf9e814d393
Tehut <tehut.m.g@gmail.com>
2017-08-23 15:46:51 -0700

    commit refactored

--- main.go
+++ main.go
@@ -1,87 +1,82 @@
-
 package main
 
 import (
-	"fmt"
-	"strings"
+	"errors"
 )
 
-type Ops interface{
+type ops interface {
 }
 
-type Write struct{
-    data string
+type write struct {
+	data string
 }
 
-type Read struct{
+// type read struct {
+// }
+
+type deleteCommand struct {
 }
 
-type Delete struct{
+type node struct {
+	// change to transaction
+	parent *node
+	ops    map[string]ops
 }
 
-
-type  Node struct {
-    parent *Node 
-	ops map[string]Ops
-   }
-
-
-type Root struct{
-	dictionary map[string]string
-	operations *Node
-
+type root struct {
+	db         map[string]string
+	operations *node
+	// change to transactions
 }
 
 // StevEx: What are your thoughts on using "this" as the reciever. I've seen some differing opinions on the internet
-        // also is the reciever a pointer to root? is that what r *root means? That r "is the type pointer to root" and when we call the method we are "passing"
-        // the currently instantiated struct?  My main point of confusion is that in newNode() we seem to be passing an argument but most other methods seem to pass 
+// also is the reciever a pointer to root? is that what r *root means? That r "is the type pointer to root" and when we call the method we are "passing"
+// the currently instantiated struct?  My main point of confusion is that in newNode() we seem to be passing an argument but most other methods seem to pass
 // ---------------------------------------------------------
+// commands should be methods others can be functions
 
-func (r *root) newNode(parent) *Node {
-        return &Node{parent:parent}
+func newNode(parent *node) *node {
+	return &node{parent: parent}
 }
 
-
-func (r *root) start() *Node {
-    var parent *Node
-        if operations == nil {
-            parent = *r
-        }else {parent = operations}
-
-        newOperations := r.newNode(operations)
-        r.operations = newOperations
+func (r *root) start() {
+	r.operations = newNode(r.operations)
 }
 
-func (r *root) fetchAncestor() *Node{
-     ancestorNode := &operations.parent
-     return ancestorNode
+func (r *root) abort() error {
+	if r.operations == nil {
+		return errors.New("You do not have a transaction open at this time")
+	}
+
+	r.operations = r.operations.parent
+	return nil
 }
 
-func (r *root) abort() *Node {
-    if operations == nil{
-        fmt.Println("You do not have a transaction open at this time")
-    }else {
-        r.operations = fetchAncestor()
-    }
+func (r *root) commit() {
+	// error if operations is nil
+	if r.operations.parent == nil {
+		// if parent is nil
+		// loop through execute operations on db
+		for key, value := range r.operations.ops {
+			// realized I could have just done an if statement after I wrote it as a switch
+			// kept it as a switch mostly to play with the syntax in go--which is almost identical to js
+			switch value.(type) {
+			case write:
+				r.db[key] = value.(write).data
+			case deleteCommand:
+				delete(r.db, key)
+			}
+		}
+	} else {
+	// loop through operations and copy to parent operations map
+		for key,value :range r.operations.ops {
+			r.operations.parent.operations[key] = value
+		}
+	}
+
+	// exit
+
 }
-
-func (r *root) commit() map[string]string{
-    currentNodeMap = r.operations
-    for key,value :range currentNodeMap {
-        // realized I could have just done an if statement after I wrote it as a switch
-        // kept it as a switch mostly to play with the syntax in go--which is almost identical to js
-        switch value {
-            case Write struct:
-            // have not checked if go allows for overwriting maps or if we should just implem
-            dictionary[key] = value.data
-            case Delete struct:
-            delete(dictionary,key) 
-        }
-    }
-}
-
-
-
 
 // Psuedocode implementation
 //     func (r *root) read(k){
@@ -91,15 +86,15 @@ func (r *root) commit() map[string]string{
 
 // 		}
 //         until current_map[k] != nil {
-            
+
 //                 func(*element)Next{
 // 					element_value =current_map[k]
-				
+
 //             }
 //         }
-	   
+
 // 		display_value = element_value
-// 		// include 
+// 		// include
 
 //     func write (k,v){
 //         current_map[k]=v
@@ -124,22 +119,21 @@ func (r *root) commit() map[string]string{
 //     }
 // }
 
-
 // func main (){
 // quit = false
 
-// while quit == false 
-	
-	// create an empty node with a pointer to nill
-    // run command prompt
-    // downcase result
-    // switch statement
-    //     read
-    //     write
-    //     delete
-    //     start
-    //     commit
-    //     abort
-	// 	quit
-		
-}
+// while quit == false
+
+// create an empty node with a pointer to nill
+// run command prompt
+// downcase result
+// switch statement
+//     read
+//     write
+//     delete
+//     start
+//     commit
+//     abort
+// 	quit
+
+// }
